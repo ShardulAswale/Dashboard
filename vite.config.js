697b96699a74b167808fb6908bef42d8bb88634e
ShardulAswale <unrealdoc12@gmail.com>
2023-10-17 12:27:16 +0530

    Deploy 4

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/Dashboard/",
-    server: {
-      watch: {
-        ignored: ["!**/node_modules/autopreview/**"],
-      },
-    },
-    // The watched package must be excluded from optimization,
-    // so that it can appear in the dependency graph and trigger hot reload.
-    optimizeDeps: {
-      exclude: ["autopreview"],
-    },
-  };
-
-  if (command !== "serve") {
-    config.base = "/Dashboard/";
-  }
-
-  return config;
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Dashboard/'
+})
