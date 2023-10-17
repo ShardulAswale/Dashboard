c8e5bdccbb2e43d8404a495e29d64c9b7c424a48
ShardulAswale <unrealdoc12@gmail.com>
2023-10-17 12:21:14 +0530

    Deploy 3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/",
+    base: "/Dashboard/",
     server: {
       watch: {
         ignored: ["!**/node_modules/autopreview/**"],
