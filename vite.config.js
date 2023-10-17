1c8cc0bc0827aa66cf6fd0eab609f50ef31b6379
ShardulAswale <unrealdoc12@gmail.com>
2023-10-17 15:53:32 +0530

    Revert "Deploy 3"
    This reverts commit c8e5bdccbb2e43d8404a495e29d64c9b7c424a48.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+<<<<<<< HEAD
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Dashboard/'
 })
+=======
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+    server: {
+      watch: {
+        ignored: ["!**/node_modules/autopreview/**"],
+      },
+    },
+    // The watched package must be excluded from optimization,
+    // so that it can appear in the dependency graph and trigger hot reload.
+    optimizeDeps: {
+      exclude: ["autopreview"],
+    },
+  };
+
+  if (command !== "serve") {
+    config.base = "/Dashboard/";
+  }
+
+  return config;
+});
+>>>>>>> parent of c8e5bdc (Deploy 3)
